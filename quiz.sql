862abb8dc03b7c03109781c6c79f8c86304be060
yneelima77 <35612917+yneelima77@users.noreply.github.com>
2023-07-23 23:50:19 -0400

    Add files via upload

---
+++ quiz.sql
@@ -0,0 +1,221 @@
+insert into questions(ques_id, title, optionA, optionB, optionC, optionD, ans, choose)
+values(
+	1, 
+	'What is a correct syntax to output "Hello World" in Java?',  
+	'echo "Hello World"',
+	'printf("Hello World")',
+	'System.out.println("Hello World")',
+    'println("Hello World)',
+	3,
+	-1)
+;
+insert into questions(ques_id, title, optionA, optionB, optionC, optionD, ans, choose)
+values(
+    2,
+    'What is the output of the following code snippet in Java? \nString s = "Hello";\nSystem.out.println(s.length());',
+    '5',
+    '6',
+    'Compilation error',
+    'Runtime error',
+    1,
+    -1)
+;
+insert into questions(ques_id, title, optionA, optionB, optionC, optionD, ans, choose)
+values(
+    3,
+    'Which loop in Java allows the code block to be executed at least once?',
+    'for loop',
+    'while loop',
+    'do-while loop',
+    'if-else statement',
+    3,
+    -1)
+;
+insert into questions(ques_id, title, optionA, optionB, optionC, optionD, ans, choose)
+values(
+    4,
+    'What is the output of the following code snippet in Java? \nint x = 5;\nSystem.out.println(x++);',
+    '5',
+    '6',
+    'Compilation error',
+    'Runtime error',
+    1,
+    -1)
+;
+insert into questions(ques_id, title, optionA, optionB, optionC, optionD, ans, choose)
+values(
+    5,
+    'What is the correct way to declare a constant in Java?',
+    'final int x = 5;',
+    'constant x = 5;',
+    'x = 5;',
+    'const x = 5;',
+    1,
+    -1)
+;
+insert into questions(ques_id, title, optionA, optionB, optionC, optionD, ans, choose)
+values(
+    6,
+    'What is the result of the expression 10 % 3 in Java?',
+    '1',
+    '3',
+    '0',
+    '10',
+    1,
+    -1)
+;
+insert into questions(ques_id, title, optionA, optionB, optionC, optionD, ans, choose)
+values(
+    7,
+    'What is the correct way to declare a variable in Java?',
+    'int x = 5;',
+    'variable x = 5;',
+    'x = 5;',
+    'var x = 5;',
+    1,
+    -1)
+;
+insert into questions(ques_id, title, optionA, optionB, optionC, optionD, ans, choose)
+values(
+    8,
+    'Which statement is used to read input from the user in Java?',
+    'readLine()',
+    'readInt()',
+    'nextLine()',
+    'next()',
+    3,
+    -1)
+;
+
+insert into questions(ques_id, title, optionA, optionB, optionC, optionD, ans, choose)
+values(
+    9,
+    'Which statement is used to exit from a loop in Java?',
+    'break',
+    'exit',
+    'return',
+    'continue',
+    1,
+    -1)
+;
+insert into questions(ques_id, title, optionA, optionB, optionC, optionD, ans, choose)
+values(
+    10,
+    'What is the output of the following code snippet in Java? \nint x = 5;\nint y = 2;\nSystem.out.println(x % y);',
+    '1',
+    '3',
+    '0',
+    '10',
+    1,
+    -1)
+;
+insert into questions(ques_id, title, optionA, optionB, optionC, optionD, ans, choose)
+values(
+    11,
+    'Which operator is used to compare two values for equality in Java?',
+    '==',
+    '=',
+    '!=',
+    '<>',
+    1,
+    -1)
+;
+insert into questions(ques_id, title, optionA, optionB, optionC, optionD, ans, choose)
+values(
+    12,
+    'What is the result of the expression 5 / 2 in Java?',
+    '2.5',
+    '2',
+    '2.0',
+    '2.25',
+    2,
+    -1)
+;
+insert into questions(ques_id, title, optionA, optionB, optionC, optionD, ans, choose)
+values(
+    13,
+    'Which data type in Java is used to store whole numbers?',
+    'int',
+    'float',
+    'double',
+    'long',
+    1,
+    -1)
+;
+insert into questions(ques_id, title, optionA, optionB, optionC, optionD, ans, choose)
+values(
+    14,
+    'What is the output of the following code snippet in Java? \nint x = 10;\nint y = ++x;\nSystem.out.println(y);',
+    '10',
+    '11',
+    '9',
+    'Compilation error',
+    2,
+    -1)
+;
+insert into questions(ques_id, title, optionA, optionB, optionC, optionD, ans, choose)
+values(
+    15,
+    'Which operator is used to perform exponentiation in Java?',
+    '^',
+    '*',
+    '/',
+    '**',
+    4,
+    -1)
+;
+insert into questions(ques_id, title, optionA, optionB, optionC, optionD, ans, choose)
+values(
+    16,
+    'What is the output of the following code snippet in Java? \nString s = "Hello";\nSystem.out.println(s.substring(1, 3));',
+    'el',
+    'ell',
+    'llo',
+    'H',
+    2,
+    -1)
+;
+insert into questions(ques_id, title, optionA, optionB, optionC, optionD, ans, choose)
+values(
+    17,
+    'Which keyword is used to define a method that does not return any value?',
+    'void',
+    'null',
+    'empty',
+    'none',
+    1,
+    -1)
+;
+insert into questions(ques_id, title, optionA, optionB, optionC, optionD, ans, choose)
+values(
+    18,
+    'What is the result of the expression true && false in Java?',
+    'true',
+    'false',
+    'Compilation error',
+    'Runtime error',
+    2,
+    -1)
+;
+insert into questions(ques_id, title, optionA, optionB, optionC, optionD, ans, choose)
+values(
+    19,
+    'What is the output of the following code snippet in Java? \nint x = 10;\nint y = x++;\nSystem.out.println(y);',
+    '10',
+    '11',
+    '9',
+    'Compilation error',
+    1,
+    -1)
+;
+insert into questions(ques_id, title, optionA, optionB, optionC, optionD, ans, choose)
+values(
+    20,
+    'Which data type in Java is used to store a single character?',
+    'char',
+    'String',
+    'int',
+    'boolean',
+    1,
+    -1)
+;
